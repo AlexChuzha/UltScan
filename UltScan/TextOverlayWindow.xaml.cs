039a5234697cecbea8ea83c8e6ce816e9162dfef
Alexander Chuzha <chuzhaalexander@gmail.com>
2026-01-30 16:53:18 +0300

    0.1.1.1 Добавлены улучшения в работе с горячими клавишами
    Esc - закрыть окно определения оверлея
    Shift+Win+Z - создать новый оверлей

--- UltScan/TextOverlayWindow.xaml.cs
+++ UltScan/TextOverlayWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace UltScan;
 
@@ -8,15 +11,43 @@ public partial class TextOverlayWindow : Window
     {
         InitializeComponent();
 
+        ShowActivated = false;
+        Focusable = false;
+
         Left = rect.Left;
         Top = rect.Top;
         Width = rect.Width;
         Height = rect.Height;
 
+        SourceInitialized += (_, __) => EnableClickThrough();
+
         Loaded += (_, __) =>
         {
-            Editor.Focus();
             Editor.CaretIndex = Editor.Text.Length;
         };
     }
+
+    private void EnableClickThrough()
+    {
+        var hwnd = new WindowInteropHelper(this).Handle;
+        if (hwnd == IntPtr.Zero)
+        {
+            return;
+        }
+
+        var exStyle = GetWindowLongPtr(hwnd, GwlExstyle);
+        var updatedStyle = new IntPtr(exStyle.ToInt64() | WsExTransparent | WsExToolwindow);
+        SetWindowLongPtr(hwnd, GwlExstyle, updatedStyle);
+    }
+
+    private const int GwlExstyle = -20;
+    private const int WsExTransparent = 0x00000020;
+    private const int WsExToolwindow = 0x00000080;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
 }
